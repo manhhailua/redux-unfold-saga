ad74ff496446c64aba662fe1998ca87fda7aeed3
Pham Tuan Manh <manhpt2@viettelpost.com.vn>
2019-06-21 14:19:04 +0700

    fix: 🐛 Remove declaration module

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,45 +2,43 @@
 import { AnyAction } from 'redux';
 import { SagaIterator } from 'redux-saga';
 
-declare module 'redux-unfold-saga' {
-  interface UnfoldSagaActionType extends AnyAction {
-    callbacks: UnfoldSagaCallbacksType;
-    options: any;
-    payload: any;
-  }
-
-  interface UnfoldSagaActionExecutionType {
-    (
-      payload: any,
-      callbacks?: UnfoldSagaCallbacksType,
-      options?: any,
-    ): UnfoldSagaActionType;
-  }
-
-  interface UnfoldSagaCallbacksType {
-    onBeginning?: Function;
-    onFailure?: Function;
-    onFinish?: Function;
-    onSuccess?: Function;
-  }
-
-  interface UnfoldSagaHandlerType {
-    handler: Function;
-    key: string;
-  }
-
-  export function createActionTypeOnBeginning(key: string): string;
-
-  export function createActionTypeOnFailure(key: string): string;
-
-  export function createActionTypeOnFinish(key: string): string;
-
-  export function createActionTypeOnSuccess(key: string): string;
-
-  export function createAction(type: string): UnfoldSagaActionExecutionType;
-
-  export function unfoldSaga(
-    type: UnfoldSagaHandlerType,
-    callback: UnfoldSagaCallbacksType,
-  ): SagaIterator;
+export interface UnfoldSagaActionType extends AnyAction {
+  callbacks: UnfoldSagaCallbacksType;
+  options: any;
+  payload: any;
 }
+
+export interface UnfoldSagaActionExecutionType {
+  (
+    payload: any,
+    callbacks?: UnfoldSagaCallbacksType,
+    options?: any,
+  ): UnfoldSagaActionType;
+}
+
+export interface UnfoldSagaCallbacksType {
+  onBeginning?: Function;
+  onFailure?: Function;
+  onFinish?: Function;
+  onSuccess?: Function;
+}
+
+export interface UnfoldSagaHandlerType {
+  handler: Function;
+  key: string;
+}
+
+export function createActionTypeOnBeginning(key: string): string;
+
+export function createActionTypeOnFailure(key: string): string;
+
+export function createActionTypeOnFinish(key: string): string;
+
+export function createActionTypeOnSuccess(key: string): string;
+
+export function createAction(type: string): UnfoldSagaActionExecutionType;
+
+export function unfoldSaga(
+  type: UnfoldSagaHandlerType,
+  callback: UnfoldSagaCallbacksType,
+): SagaIterator;
